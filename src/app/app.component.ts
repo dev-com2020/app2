d978a2241023361c28626209b181f170ecb6b386
dev-com2020 <kwp@tlen.pl>
2023-03-28 14:45:34 +0200

    28.03 g. 14:45

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app2';
+
+
+  title$ = new Observable(observer => {
+    setInterval(() => {
+      observer.next();
+    }, 2000);
+});
+
+
+  private setTitle = () => {
+    const timestamp = new Date().getMilliseconds();
+    document.title = `App2 ${timestamp}`;
+  }
+
+  // private changeTitle(callback: Function){
+  //   setTimeout(() => {
+  //     callback();
+  //   }, 2000);
+  // }
+
+//   private onComplete(){
+//     return new Promise<void>(resolve => {
+//       setInterval(() => {
+//         resolve();
+//       }, 2000);
+//   });
+// }
+
+  constructor(){
+    this.title$.subscribe(this.setTitle);
+}
 }
